029eea74707fbe0008310981f6651272693904fb
Atul Jain <atuljain10595@gmail.com>
2014-12-18 22:38:41 +0530

    Rules from backend.
    Rules are added using jQuery array.

--- Categories/Ego WordPress Theme by UDTHEMES_files/myJs.js
+++ Categories/Ego WordPress Theme by UDTHEMES_files/myJs.js
@@ -3,9 +3,11 @@ jQuery(document).ready(function($){
     
     
     var menuIn = false;
-    $rc = $(".round-thumb > a");
+    $rc = $(".round-thumb");
     $mL = $(".modalL");
     $mR = $(".modalR");
+    
+    var rulesID523 = ["ONE","Rule2 Rule2"];
     
      $cross = $(".spanExitButton");
     $cross.addClass("hideMe");
@@ -15,7 +17,18 @@ jQuery(document).ready(function($){
  }
     
     $rc.click(function(){
-         setTimeout(showpanel,2500);
+        
+        if(this.id==535){
+            $list = $mL.find("ol");
+            //alert(rulesID523.length);
+            for(i=0;i<rulesID523.length;)
+            {
+                $list.find("li:nth-of-type("+(i+1)+")").text(rulesID523[i]);
+                i++;
+            }
+        }
+        
+         setTimeout(showpanel,2000);
         $mR.addClass("animR");
         $mL.addClass("animL");
         $('#wrapper').addClass("lightsDim");
@@ -30,4 +43,6 @@ jQuery(document).ready(function($){
     });
   
    
+    
+    
 });
