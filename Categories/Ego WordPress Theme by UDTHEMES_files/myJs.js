bc426753f44080495fda968d0198d7345d524ad7
Atul Jain <atuljain10595@gmail.com>
2014-12-18 21:02:52 +0530

    Pointer fixed.
    Cursor fixed; Exit button positioned and hidden init.

--- Categories/Ego WordPress Theme by UDTHEMES_files/myJs.js
+++ Categories/Ego WordPress Theme by UDTHEMES_files/myJs.js
@@ -7,15 +7,23 @@ jQuery(document).ready(function($){
     $mL = $(".modalL");
     $mR = $(".modalR");
     
-  
+     $cross = $(".spanExitButton");
+    $cross.addClass("hideMe");
+    
+    function showpanel() {     
+  $cross.toggleClass("hideMe");
+ }
+    
     $rc.click(function(){
-        
+         setTimeout(showpanel,2500);
         $mR.addClass("animR");
         $mL.addClass("animL");
         $('#wrapper').addClass("lightsDim");
+        
     });
-   $cross = $(".spanExitButton");
+
     $cross.click(function(){
+         showpanel();
          $mR.removeClass("animR");
         $mL.removeClass("animL");
         $('#wrapper').removeClass("lightsDim");
