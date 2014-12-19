a17e76fdd309fa1a95ec37747f62afaa7da9220f
Atul Jain <atuljain10595@gmail.com>
2014-12-19 18:24:59 +0530

    modals Beautification

--- Categories/Ego WordPress Theme by UDTHEMES_files/myJs.js
+++ Categories/Ego WordPress Theme by UDTHEMES_files/myJs.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function($){
     $rc = $(".round-thumb");
     $mL = $(".modalL");
     $mR = $(".modalR");
+    $modalListOfRules = $(".orderedList");
     
     var rulesID523 = [
         "ONE",
@@ -36,35 +37,55 @@ jQuery(document).ready(function($){
      $cross = $(".spanExitButton");
     $cross.addClass("hideMe");
     
+    function exitModal(){
+         showpanel();
+         $mR.removeClass("animR");
+        $mL.removeClass("animL");
+        $('#wrapper').removeClass("lightsDim");
+        $("#wrapper").unbind("click",exitModal);
+        $modalListOfRules.empty();
+        
+    }
+    
+    
     function showpanel() {     
   $cross.toggleClass("hideMe");
+//        $('body:not("modal")')
  }
+    function exitOnclickAnywhere(){
+    $("#wrapper").click(exitModal);
+    }
     
     $rc.click(function(){
         
+        
+        
         if(this.id==535){
             $list = $mL.find("ol");
+            $(".bgRules").css({
+                "background": "url('"+"./slide-122.jpg"+"') no-repeat"
+              
+                });
+            
+    
             //alert(rulesID523.length);
             for(i=0;i<rulesID523.length;)
             {
-                $list.append("<li>"+rulesID523[i]+"</li>");
+                $list.append("<li style='color:rgba(255,255,255,1);'>"+(i+1)+".&nbsp;&nbsp;"+rulesID523[i]+"</li>");
                 i++;
             }
+            $(".nano").nanoScroller();
         }
         
-         setTimeout(showpanel,2000);
+         setTimeout(showpanel,1900);
+        setTimeout(exitOnclickAnywhere,2000);
         $mR.addClass("animR");
         $mL.addClass("animL");
         $('#wrapper').addClass("lightsDim");
         
     });
 
-    $cross.click(function(){
-         showpanel();
-         $mR.removeClass("animR");
-        $mL.removeClass("animL");
-        $('#wrapper').removeClass("lightsDim");
-    });
+    $cross.click(exitModal);
   
    
     
